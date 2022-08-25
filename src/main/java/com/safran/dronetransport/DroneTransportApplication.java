fb062e8117404367396467759d77a6a670aeadc6
safranfaiz@yahoo.com <safranfaiz@yahoo.com>
2022-08-26 02:14:27 +0530

    git ignore updated

---
+++ src/main/java/com/safran/dronetransport/DroneTransportApplication.java
@@ -0,0 +1,13 @@
+package com.safran.dronetransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneTransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneTransportApplication.class, args);
+	}
+
+}
